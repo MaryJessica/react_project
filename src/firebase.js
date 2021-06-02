eba43a2fe821a1c4a2a6412e93b3bb1f8193f350
MaryJessica <77490616+MaryJessica@users.noreply.github.com>
2021-06-02 16:51:20 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase.initializeApp ({
+    apiKey: "AIzaSyDoec1bi4znBXFpnLcouPA7Y6bHrysMY2g",
+    authDomain: "unichat-5deee.firebaseapp.com",
+    projectId: "unichat-5deee",
+    storageBucket: "unichat-5deee.appspot.com",
+    messagingSenderId: "662461806938",
+    appId: "1:662461806938:web:830f88d137c9da3ac35b63"
+  }).auth();
+
+ export default  firebase;
